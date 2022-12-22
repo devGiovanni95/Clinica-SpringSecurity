415cae9e3533e0d1dc9e66e475892135814a34dc
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-12-21 22:06:06 -0300

    Created configs for the HTTP Requests

--- src/main/java/com/giovanni/curso/security/config/SecurityConfig.java
+++ src/main/java/com/giovanni/curso/security/config/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.giovanni.curso.security.config;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       // super.configure(http);
+        http.authorizeHttpRequests()
+                .antMatchers("/webjars/**","/css/**","/js/**").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+    }
 }
